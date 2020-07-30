e3dfa1624e3bef7d341802c6762f13a9394fc192
Jacob Galam <hgecdkto@gmail.com>
2020-07-30 16:28:37 +0300

    make more order with filles

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
-import * as yup from "yup";
-import { Formik, Form, Field } from "formik";
-import { TextFormField } from "./FormFields/TextField";
-import { EmailPass } from "./FormFields/EmaiPassl";
-import { Username } from "./FormFields/Username";
-
+import { EmailPass } from "./components/EmailPassl";
+import { Username } from "./components/Username";
+import { Typography } from '@material-ui/core';
 
 const App = () => {
   return (
-    <div style={{ textAlign: "center" }}>
-      <div style={{ width: 300, margin: "auto" }}>
-        <Username></Username>
-        <EmailPass></EmailPass>
-      </div>
-    </div >
+    <div>
+      <Typography>Register</Typography>
+      <Username></Username>
+      <EmailPass></EmailPass>
+    </div>
   );
 };
 
